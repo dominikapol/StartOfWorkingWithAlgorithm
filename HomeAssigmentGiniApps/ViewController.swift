81d090b67dcb50ec3445e9d74a9108201cbe3da0
Dominika <dominikapiganova@mail.ru>
2022-01-17 16:03:35 +0200

    Initial Commit

---
+++ HomeAssigmentGiniApps/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeAssigmentGiniApps
+//
+//  Created by Dominika Poleshyck on 17.01.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
